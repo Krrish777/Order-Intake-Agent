0bbfd6feb56b5d55f761f375f7990e13a543bdba
Krrish777 <777krrish@gmail.com>
2026-04-25 14:22:38 +0530

    feat(track-a2): add GMAIL_SEND_SCOPE + A2_SCOPES

--- backend/gmail/scopes.py
+++ backend/gmail/scopes.py
@@ -6,7 +6,14 @@ A3 (deploy):   no additional scope - watch uses the same subset
 """
 
 GMAIL_MODIFY_SCOPE = "https://www.googleapis.com/auth/gmail.modify"
+GMAIL_SEND_SCOPE = "https://www.googleapis.com/auth/gmail.send"
 
 A1_SCOPES = [GMAIL_MODIFY_SCOPE]
+A2_SCOPES = A1_SCOPES + [GMAIL_SEND_SCOPE]
 
-__all__ = ["GMAIL_MODIFY_SCOPE", "A1_SCOPES"]
+__all__ = [
+    "GMAIL_MODIFY_SCOPE",
+    "GMAIL_SEND_SCOPE",
+    "A1_SCOPES",
+    "A2_SCOPES",
+]
