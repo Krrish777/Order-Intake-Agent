086a4e0ac14b4b961fb8f03c680435d78c85c146
Krrish777 <777krrish@gmail.com>
2026-04-25 14:06:40 +0530

    feat(track-a1): add GmailPoller async loop

--- backend/gmail/__init__.py
+++ backend/gmail/__init__.py
@@ -1,11 +1,13 @@
 """Gmail integration package (Track A1 ingress side)."""
 from backend.gmail.adapter import gmail_message_to_envelope
 from backend.gmail.client import GmailClient
+from backend.gmail.poller import GmailPoller
 from backend.gmail.scopes import A1_SCOPES, GMAIL_MODIFY_SCOPE
 
 __all__ = [
     "A1_SCOPES",
     "GMAIL_MODIFY_SCOPE",
     "GmailClient",
+    "GmailPoller",
     "gmail_message_to_envelope",
 ]
